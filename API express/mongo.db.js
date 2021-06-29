b107283c941b7e60f020cc0e8cec74cf35c83868
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2021-06-29 10:48:19 +0200

    Revert "delete this pls"
    This reverts commit d9eb732e577ebaec5e3705d96359f26392793157.

--- API express/mongo.db.js
+++ API express/mongo.db.js
@@ -1,13 +1,13 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require('mongoose');
+exports.__esModule = true;
+var mongoose = require('mongoose');
 require('dotenv').config();
 //const mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-const mongoUrl = `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}?readPreference=primary&authSource=admin&appname=MongoDB%20Compass&ssl=false`;
+var mongoUrl = "mongodb://" + process.env.MONGO_DB_HOST + ":" + process.env.MONGO_DB_PORT + "/" + process.env.MONGO_DB_NAME + "?readPreference=primary&authSource=admin&appname=MongoDB%20Compass&ssl=false";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function () {
     console.log('MongoDB connected');
 });
-exports.default = db;
+exports["default"] = db;
