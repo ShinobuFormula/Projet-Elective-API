d9eb732e577ebaec5e3705d96359f26392793157
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2021-06-29 10:47:18 +0200

    delete this pls

--- API express/mongo.db.js
+++ API express/mongo.db.js
@@ -1,13 +1,13 @@
 "use strict";
-exports.__esModule = true;
-var mongoose = require('mongoose');
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require('mongoose');
 require('dotenv').config();
 //const mongoUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-var mongoUrl = "mongodb://" + process.env.MONGO_DB_HOST + ":" + process.env.MONGO_DB_PORT + "/" + process.env.MONGO_DB_NAME + "?readPreference=primary&authSource=admin&appname=MongoDB%20Compass&ssl=false";
+const mongoUrl = `mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}?readPreference=primary&authSource=admin&appname=MongoDB%20Compass&ssl=false`;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function () {
     console.log('MongoDB connected');
 });
-exports["default"] = db;
+exports.default = db;
